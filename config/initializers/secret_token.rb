32642bb9b017bf503e89b9d358c4f688f1e21e01
John Rees <john@bitsushi.com>
2013-10-19 10:03:41 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+M60carwarehouse::Application.config.secret_key_base = 'dde4f9bfe0e4c6c6b5c887e8f6347f58b3d688975d69ea865f4f2b6bc64084eed6460e20d1170e211b5c7f6cf231d3fd7f85438e8a2275f064f47db830e5ae87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+M60carwarehouse::Application.config.session_store :cookie_store, key: '_m60carwarehouse_session'
